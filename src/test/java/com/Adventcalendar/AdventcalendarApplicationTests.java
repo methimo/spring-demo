b4825b5a19e9494d59f2dd4ae16732022aca31cf
tachi <tachi@test.com>
2020-11-25 14:58:24 +0900

    Initial commit

---
+++ src/test/java/com/Adventcalendar/AdventcalendarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Adventcalendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdventcalendarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
